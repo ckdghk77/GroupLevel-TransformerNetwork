36d3cfccccdf7f602c024b5e6706d4a974e683bc
Lee Chang Hwa <ckdghk777@gmail.com>
2020-06-11 22:59:51 +0900

    Add files via upload

--- plot_interpol.py
+++ plot_interpol.py
@@ -23,51 +23,50 @@ train_x = np.load(file_train);
 
 plt.rcParams["figure.figsize"] = (7.85, 7.9);
 fig_count = 0;
-for src in range(generated.shape[0]) :
 
-    for src in range(generated.shape[0]):  # src
+for src in range(generated.shape[0]):  # src
 
-        for tar in range(generated.shape[2]):  # target
-            if tar == 0:
-                continue;
+    for tar in range(generated.shape[2]):  # target
+        if tar == 0:
+            continue;
 
-            total_row_num = generated.shape[0];
-            total_col_num = generated.shape[0];
+        total_row_num = 5;
+        total_col_num = 5;
 
-            fig, axes = plt.subplots(total_row_num,
-                                     total_col_num,
-                                     gridspec_kw={'wspace': 0, 'hspace': 0});
+        fig, axes = plt.subplots(total_row_num,
+                                 total_col_num,
+                                 gridspec_kw={'wspace': 0, 'hspace': 0});
 
-            for idx in range(generated.shape[1] + 2):
+        for idx in range(generated.shape[1] + 2):
 
-                cur_row = idx // total_row_num;
-                cur_col = idx % total_row_num;
-                axes[cur_row, cur_col].axis('off')
-                if idx == 0:  # src_img
-                    if train_x[src, 0].shape[0]==3 :
-                        axes[cur_row, cur_col].imshow(np.transpose(train_x[src, 0], [1, 2, 0]));
-                    else :
-                        axes[cur_row, cur_col].imshow(np.squeeze(train_x[src, 0]),cmap='Greys');
+            cur_row = idx // total_row_num;
+            cur_col = idx % total_row_num;
+            axes[cur_row, cur_col].axis('off')
+            if idx == 0:  # src_img
+                if train_x[src, 0].shape[0]==3 :
+                    axes[cur_row, cur_col].imshow(np.transpose(train_x[src, 0], [1, 2, 0]));
+                else :
+                    axes[cur_row, cur_col].imshow(np.squeeze(train_x[src, 0]),cmap='Greys');
 
 
-                elif idx == generated.shape[1] + 1:
-                    if train_x[src, tar].shape[0]==3:
-                        axes[cur_row, cur_col].imshow(np.transpose(train_x[src, tar], [1, 2, 0]));
-                    else :
-                        axes[cur_row, cur_col].imshow(np.squeeze(train_x[src, tar]),cmap='Greys');
-                else:
-                    if generated[src, idx - 1, tar].shape[0]==3:
-                        axes[cur_row, cur_col].imshow(
-                            np.transpose(generated[src, idx - 1, tar], [1, 2, 0]));
-                    else :
-                        axes[cur_row, cur_col].imshow(np.squeeze(generated[src, idx - 1, tar]), cmap='Greys');
+            elif idx == generated.shape[1] + 1:
+                if train_x[src, tar].shape[0]==3:
+                    axes[cur_row, cur_col].imshow(np.transpose(train_x[src, tar], [1, 2, 0]));
+                else :
+                    axes[cur_row, cur_col].imshow(np.squeeze(train_x[src, tar]),cmap='Greys');
+            else:
+                if generated[src, idx - 1, tar].shape[0]==3:
+                    axes[cur_row, cur_col].imshow(
+                        np.transpose(generated[src, idx - 1, tar], [1, 2, 0]));
+                else :
+                    axes[cur_row, cur_col].imshow(np.squeeze(generated[src, idx - 1, tar]), cmap='Greys');
 
-            plt.axis('off')
-            #plt.savefig("result.jpg", format='jpg', dpi=1200)
-            plt.savefig(os.path.join(args.load_folder,"result" +str(fig_count)+ ".jpg"), format='jpg')
-            plt.close('all')
-            fig_count+=1;
-            #plt.show()
+        plt.axis('off')
+        #plt.savefig("result.jpg", format='jpg', dpi=1200)
+        plt.savefig(os.path.join(args.load_folder,"result" +str(fig_count)+ ".jpg"), format='jpg')
+        plt.close('all')
+        fig_count+=1;
+        #plt.show()
 
 
 
